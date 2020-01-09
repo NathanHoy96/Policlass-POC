e56eb62f2c882ca24bea023167501d80a61a80f7
Nathan Hoy <nathan_hoy@live.co.uk>
2020-01-09 23:07:56 +0000

    Recreating legacy files

---
+++ Android/app/src/main/java/com/example/policlass_poc/GetClassification.java
@@ -0,0 +1,61 @@
+package com.example.policlass_poc;
+
+import android.os.AsyncTask;
+import android.text.LoginFilter;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+public class GetClassification extends AsyncTask<String,Void,String> {
+
+    private int serverPort = 8000;
+    private String serverIP = "35.178.29.132";
+    private Socket socket;
+    private PrintWriter clientOutput;
+    private BufferedReader clientInput;
+    MainActivity main;
+
+    private String response;
+
+    GetClassification(MainActivity m)
+    {
+        this.main = m;
+    }
+
+    @Override
+    protected String doInBackground(String... voids) {
+
+        try {
+            Log.i("Progress", "Executing thread");
+            socket = new Socket(serverIP,serverPort);
+            Log.i("Connected to: ",socket.getRemoteSocketAddress().toString());
+            clientOutput = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8), true);
+            clientInput = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
+            clientOutput.println(voids[0]);
+            Log.i("Message state:","Sent");
+            response = clientInput.readLine();
+            Log.i("Response: ",response);
+            socket.close();
+            clientOutput.close();
+            clientInput.close();
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return response;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        main.asyncResult(s);
+    }
+}
+
--- Android/app/src/main/java/com/example/policlass_poc/MainActivity.java
+++ Android/app/src/main/java/com/example/policlass_poc/MainActivity.java
@@ -3,12 +3,37 @@ package com.example.policlass_poc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText urlEditText;
+    private TextView classifcationDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void On_Classify_Click(View view) {
+
+        urlEditText = findViewById(R.id.url_input);
+        classifcationDisplay = findViewById(R.id.classification_text);
+
+        String url = urlEditText.getText().toString();
+        Log.i("URL Entry: ",url);
+
+        GetClassification getClassification = new GetClassification(this);
+        getClassification.execute(url);
+    }
+
+    void asyncResult(String result) {
+        //This method is called when AsyncTask posts its result. Do you stuff here
+        classifcationDisplay = findViewById(R.id.classification_text);
+        classifcationDisplay.setText(result.toUpperCase());
     }
 }
