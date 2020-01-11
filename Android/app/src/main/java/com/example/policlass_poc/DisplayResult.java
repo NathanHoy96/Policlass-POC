fd0e38f8b3d790a61fc9b71c3bc995b55355377a
Nathan Hoy <nathan_hoy@live.co.uk>
2020-01-11 14:04:32 +0000

    Adding the ListView to host the alternative articles

--- Android/app/src/main/java/com/example/policlass_poc/DisplayResult.java
+++ Android/app/src/main/java/com/example/policlass_poc/DisplayResult.java
@@ -5,14 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class DisplayResult extends AppCompatActivity {
+
+    private TextView classificationText;
+    private ListView alternativesListView;
+    private ArrayList<String> alternativesList;
+    private ArrayAdapter<String> arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_result);
+
+        classificationText = findViewById(R.id.classification_text);
+        alternativesListView = findViewById(R.id.alternative_list);
+        alternativesList = new ArrayList<String>();
+        arrayAdapter = new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_spinner_item,alternativesList);
 
         Intent intent = getIntent();
         String action = intent.getAction();
